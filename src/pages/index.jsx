48599d2e3026c8b3c6c31ebb3dcd7a480d970399
Yuvraj Chandra <Singhyuvraj179@gmail.com>
2023-01-28 16:19:47 +0530

    updated 2nd section image

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -93,7 +93,7 @@ export default function HomePage() {
 						</div>
 					</div>
 					<img
-						src="https://user-images.githubusercontent.com/53931942/215045536-27cad731-d419-4323-8148-e2cbf4c569ce.jpg"
+						src="https://user-images.githubusercontent.com/53931942/215262507-3dc80479-3f2d-43dc-b412-df2686f10d5e.jpg"
 						alt=""
 						className="max-w-full aspect-[3/2] lg:aspect-square object-cover col-span-12 my-10 md:mb-0 md:mt-14 lg:my-0 lg:col-span-5 rounded-xl shadow-lg lg:h-full order-1"
 					/>
