63b9c3d1138678cff68746bc8e7cad752edd8d65
Pushpit <pushpit07@gmail.com>
2023-03-24 09:23:56 +0530

    Deployment try
    Signed-off-by: Pushpit <pushpit07@gmail.com>

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -8,6 +8,7 @@ const dbConnect = async () => {
 	try {
 		await mongoose
 			.connect(process.env.MONGODB_URI, {
+				keepAlive: true,
 				useNewUrlParser: true,
 				useUnifiedTopology: true,
 			})
