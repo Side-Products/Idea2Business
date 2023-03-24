0aa73dd3b5e0083210f989d3d353ce15f76a7b8b
Pushpit <pushpit07@gmail.com>
2023-03-24 09:45:23 +0530

    Deployment try
    Signed-off-by: Pushpit <pushpit07@gmail.com>

--- src/lib/mongodb.js
+++ src/lib/mongodb.js
@@ -6,28 +6,68 @@ if (!process.env.MONGODB_URI) {
 }
 
 const uri = process.env.MONGODB_URI;
-const options = {};
+let options = {};
 
 let client;
 let clientPromise;
 
-async function connectToDatabase() {
-	if (process.env.NODE_ENV === "development") {
-		// In development mode, use a global variable so that the value
-		// is preserved across module reloads caused by HMR (Hot Module Replacement).
-		if (!global._mongoClientPromise) {
-			client = new MongoClient(uri, options);
-			global._mongoClientPromise = await client.connect();
-		}
-		clientPromise = global._mongoClientPromise;
-	} else {
-		// In production mode, it's best to not use a global variable.
+if (process.env.NODE_ENV === "development") {
+	// In development mode, use a global variable so that the value
+	// is preserved across module reloads caused by HMR (Hot Module Replacement).
+	if (!global._mongoClientPromise) {
 		client = new MongoClient(uri, options);
-		clientPromise = await client.connect();
+		global._mongoClientPromise = client.connect();
 	}
+	clientPromise = global._mongoClientPromise;
+} else {
+	options = { ssl: true, keepAliveInitialDelay: 300000, connectTimeoutMS: 300000, socketTimeoutMS: 300000 };
+	// In production mode, it's best to not use a global variable.
+	client = new MongoClient(uri, options);
+	clientPromise = client.connect();
 }
-connectToDatabase();
 
 // Export a module-scoped MongoClient promise. By doing this in a
 // separate module, the client can be shared across functions.
 export default clientPromise;
+
+// // This approach is taken from https://github.com/vercel/next.js/tree/canary/examples/with-mongodb
+// import { MongoClient } from "mongodb";
+// import mongoose from "mongoose";
+
+// if (!process.env.MONGODB_URI) {
+// 	throw new Error('Invalid/Missing environment variable: "MONGODB_URI"');
+// }
+
+// const uri = process.env.MONGODB_URI;
+// const options = {};
+
+// let client;
+// let clientPromise;
+
+// if (process.env.NODE_ENV === "development") {
+// 	// In development mode, use a global variable so that the value
+// 	// is preserved across module reloads caused by HMR (Hot Module Replacement).
+// 	if (!global._mongoClientPromise) {
+// 		// client = new MongoClient(uri, options);
+// 		// global._mongoClientPromise = client.connect();
+// 		global._mongoClientPromise = mongoose.connect(process.env.MONGODB_URI, {
+// 			keepAlive: true,
+// 			useNewUrlParser: true,
+// 			useUnifiedTopology: true,
+// 		});
+// 	}
+// 	clientPromise = global._mongoClientPromise;
+// } else {
+// 	// In production mode, it's best to not use a global variable.
+// 	// client = new MongoClient(uri, options);
+// 	// clientPromise = client.connect();
+// 	clientPromise = mongoose.connect(process.env.MONGODB_URI, {
+// 		keepAlive: true,
+// 		useNewUrlParser: true,
+// 		useUnifiedTopology: true,
+// 	});
+// }
+
+// // Export a module-scoped MongoClient promise. By doing this in a
+// // separate module, the client can be shared across functions.
+// export default clientPromise;
