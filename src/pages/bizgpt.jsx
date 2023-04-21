4f4e4ab2b5001640552361afd20653c339aafc72
Pushpit <pushpit07@gmail.com>
2023-04-21 16:58:21 +0530

    Added  basic layout
    Signed-off-by: Pushpit <pushpit07@gmail.com>

---
+++ src/pages/bizgpt.jsx
@@ -0,0 +1,9 @@
+import PageWrapper from "@/layout/PageWrapper";
+
+export default function ContactUs() {
+	return (
+		<PageWrapper>
+			<h1 className="text-[40px] sm:text-[60px] font-bold text-center tracking-[-1px] text-gradient-primary-tr leading-[1.2em]">BizGPT</h1>
+		</PageWrapper>
+	);
+}
